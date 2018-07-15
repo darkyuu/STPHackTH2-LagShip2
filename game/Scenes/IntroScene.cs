38313c604f572f22f4b64669b206efae1d126727
Durongrit Hirunked <darkyuu@hotmail.com>
2018-07-15 13:11:02 +0700

    ADD: BGM (same song but use lower decibel) on Intro Scene

--- game/Scenes/IntroScene.cs
+++ game/Scenes/IntroScene.cs
@@ -3,13 +3,17 @@ using System;
 
 public class IntroScene : Node2D
 {
+	private AudioStreamPlayer bgm;
     public override void _Ready()
     {
-
+		bgm = GetNode("BGM") as AudioStreamPlayer;
+		bgm.Stop();
+		bgm.Play();
     }
 
 	public void OnStartGameButtonPressed()
 	{
+		bgm.Stop();
 		GetTree().ChangeScene("res://Scenes/GameplayScene.tscn");
 	}
 }
